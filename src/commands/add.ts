31c464da375d48c21a4e3ae4bf7db3634c98b313
Rafhael <rafhael.brum@gmail.com>
2025-03-31 11:13:07 -0300

    imports/exports base

---
+++ src/commands/add.ts
@@ -0,0 +1,3 @@
+export default function logMessage(): void {
+    console.log("addTask");
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import addTask from "./commands/add";
+import listTask from "./commands/list";
+import updateTask from "./commands/update";
+import deleteTask from "./commands/delete";
+
+console.log(process.argv);
+
